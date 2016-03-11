c7453d7c847b9b0cbf880c273645c78e55245a83
江欣华 <1149949564@qq.com>
2016-03-11 14:49:19 +0800

    Initial Commit

---
+++ XHAdScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XHAdScrollView
+//
+//  Created by 江欣华 on 16/3/11.
+//  Copyright © 2016年 jxh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
