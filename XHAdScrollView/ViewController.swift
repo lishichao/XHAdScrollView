eea76f38e7d539b3254d24b4c616a84a68935c5e
huahua0809 <1149949564@qq.com>
2016-03-18 10:40:27 +0800

    广告轮播图
    广告轮播图

--- XHAdScrollView/ViewController.swift
+++ XHAdScrollView/ViewController.swift
@@ -12,14 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let frame = CGRectMake(0, 20, view.bounds.width, view.bounds.width*0.6)
+        let imageView = ["2.jpg","3.jpg","4.jpg"]
+        
+        let loopView = XHAdLoopView(frame: frame, images: imageView, autoPlay: true, delay: 3, isFromNet: false)
+        loopView.delegate = self
+        
+        view.addSubview(loopView)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
+
+//遵循协议代理，调用代理方法
+extension ViewController : XHAdLoopViewDelegate {
+    func adLoopView(adLoopView: XHAdLoopView, IconClick index: NSInteger) {
+        print(index)
+    }
+}
+
 
